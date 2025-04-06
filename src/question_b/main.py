41bfcad349741eb73f91d9fbc648b594d07429b3
vencoop45 <ven.cooper@g.austincc.edu>
2025-04-06 03:32:17 +0000

    Question 2

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,29 @@
 #add import
+from question_b import get_random_number
+
+def main():
+    while True:
+        secret_number = get_random_number()
+        guesses = 0
+        print("\nI'm thinking of a number 1-5, try to guess it!")
+
+        while True:
+            try:
+                guess_str = input("Please enter your guess - or 'quit' to exit game:")
+                if guess_str.lower() == 'quit':
+                    break
+
+                guess = int(guess_str)
+                guesses += 1
+
+                if guess == secret_number:
+                    print(f"Congratulations! You guessed the number {secret_number} in {guesses} tries.")
+                    break
+                else:
+                    print("That isn't what I was thinking of. Try again!")
+            except ValueError as e:
+                print("Invalid input, enter whole number or 'quit'.")
+                
+        if guess_str.lower() == 'quit':
+            print("Thanks for playing!")
+            break
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -3,11 +3,15 @@ import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
 from src.question_a.question_a import get_person_category
+from src.question_b.question_b import get_random_number
 
 class Test_Config(unittest.TestCase):
 
     def test_get_person_category(self):
-        self.assertEqual(get_person_category(1), infant)
-        self.assertEqual(get_person_category(2), child)
-        self.assertEqual(get_person_category(14), teenager)
-        self.assertEqual(get_person_category(20), adult)
+        self.assertEqual(get_person_category(1), "infant")
+        self.assertEqual(get_person_category(2), "child")
+        self.assertEqual(get_person_category(14), "teenager")
+        self.assertEqual(get_person_category(20), "adult")
+
+    def test_get_random_number(self):
+        self.assertEqual(get_random_number)
