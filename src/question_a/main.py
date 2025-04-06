681e867e372f518893419b56616cbfb0efbb81d2
vencoop45 <ven.cooper@g.austincc.edu>
2025-04-06 02:54:21 +0000

    Question 1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,14 @@
 #add import
+from question_a import get_person_category
+def main():
+    while True:
+        try:
+            age_str = input("Please enter individual's age - or type 'quit' to exit:")
+            if age_str.lower() == 'quit':
+                break
+            age = int(age_str)
+            category = get_person_category(age)
+            print(f"The individual is a(n) {category}.")
+
+if __name__ == '__main__':
+   main() 
